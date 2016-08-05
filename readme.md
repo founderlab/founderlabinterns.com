e312d2fe1c5cf9f3fa046e863ad7eab1359cdf54
Jeremy Fu <imjeremyhi@gmail.com>
2016-08-05 10:08:42 +1000

    first commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# FounderLab base project for web apps
+
+A single page Node app with user login. Use this when starting a new project.
+
+Uses React, Redux, BackboneORM, Bootstrap, Webpack.
+
+###Changelog:
+
+
+## [0.3.0]
+ webpack + layout updates
+
+## [0.2.0]
+ fl-admin
+
+## [0.1.1]
+ React 0.14
+
+## [0.1.0]
+ Functioning app with auth
+
+## [0.0.1]
+ Initial version, probably broken in some way
---
+++ shared/reducer.js
@@ -0,0 +1,18 @@
+import Immutable from 'immutable'
+import {combineReducers} from 'redux'
+import {routerStateReducer as router} from 'redux-router'
+import {reducer as form} from 'redux-form'
+import {reducer as auth} from 'fl-auth-redux'
+import {reducer as admin} from 'fl-admin'
+import app from './modules/app/reducer'
+import profiles from './modules/users/profileReducer'
+
+export default combineReducers({
+  auth,
+  form,
+  router,
+  admin,
+  app,
+  profiles,
+  config: (state=new Immutable.Map()) => state,
+})
