873c071de46904003266732ab5a706b799e604f6
EsthervdG <esthervdgiessen@hotmail.com>
2016-09-02 11:07:11 +1000

    Added a new reporting page

---
+++ shared/modules/app/components/Reporting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Landing extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Test</h1>
+        </header>
+      </div>
+    )
+  }
+}
